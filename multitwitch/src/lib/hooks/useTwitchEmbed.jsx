4c1b45cf7c59971317badac9544a855dad50d885
Andrew <drejsp@gmail.com>
2024-09-11 00:08:18 -0400

    Put ID and URL in same file instead of passing it as props, loading the api issue?

--- multitwitch/src/lib/hooks/useTwitchEmbed.jsx
+++ multitwitch/src/lib/hooks/useTwitchEmbed.jsx
@@ -1,23 +1,27 @@
 import { useEffect, useState } from 'react';
+const twitchEmbedID = "twitch-embed";
+const twitchEmbedURL = "https://player.twitch.tv/js/embed/v1.js";
 // Dynamically load streams 
-function useTwitchEmbed({scriptURL}) {
+function useTwitchEmbed() {
     const [isScriptLoaded, setIsScriptLoaded] = useState(false);
 
     useEffect(() => {
-        const currentScript = document.querySelector('script[src="${scriptURL}"]');
-        // if the script does not exist in the DOM yet then we add it 
-        if (!currentScript) {
-            const script = document.createElement("script");
-            script.src = scriptURL;
-            script.async = true;
-            script.onload = () => setIsScriptLoaded(true);
-            script.onerror = () => console.error('Error loading Twitch script');
-            document.body.appendChild(script);
-        }
-        else {
+        // Check if element does not exist
+        const script = document.createElement("script");
+        script.src = twitchEmbedURL;
+        script.id = twitchEmbedID;
+        script.async = true;
+        // on successful load
+        script.onload = () => {
+            console.log("Twitch script loaded successfully");
             setIsScriptLoaded(true);
         }
-    }, [scriptURL]);
+        script.onerror = () => console.error('Error loading Twitch script');
+        // add script to doc body
+        document.body.appendChild(script);
+        return () => document.body.removeChild(script);
+        
+    }, []);
 
     return isScriptLoaded;
 } 
