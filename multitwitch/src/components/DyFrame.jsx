8603321cc7709a6c04740ed7ab658e6abec0f6b3
Andrew <drejsp@gmail.com>
2024-09-11 00:06:26 -0400

    Still trying to make twitch embed work

--- multitwitch/src/components/DyFrame.jsx
+++ multitwitch/src/components/DyFrame.jsx
@@ -1,34 +1,31 @@
-import { useEffect, useRef } from 'react';
+import { Component, useEffect, useRef } from 'react';
 import useTwitchEmbed from '../lib/hooks/useTwitchEmbed';
-const twitchEmbedURL = "https://player.twitch.tv/js/embed/v1.js";
 
-function Dyframe({streamID, options}) {
+function Dyframe({ streamID , options }) {
     const embedRef = useRef(null);
-    const isScriptLoaded = useTwitchEmbed(twitchEmbedURL);
-
+    const isScriptLoaded = useTwitchEmbed();
+    // on mount check if script is loaded and Twitch player exists
     useEffect(() => {
-        if (isScriptLoaded && window.Twitch) {
-            const player = window.Twitch.Player(embedRef.current, {
+        // from here create twitch player
+        if (isScriptLoaded && Twitch) {
+            console.log('Twitch player is being created');
+            const player = new Twitch.Player(embedRef.current, {
                 channel: streamID,
+                parent: ['localhost', '127.0.0.1'],
                 ...options,
             });
 
             return () => {
+                console.log('Destroying player');
                 player.destroy();
             };
         }
-    }, [isScriptLoaded, streamID, options]);
+    }, [streamID, options]);
     return (
         <div
             className='dyframe-container'
             ref={embedRef}
-            style={{
-                width: '100%',
-                height: '100%',
-                position: 'relative',
-            }}
-        >
-        </div>
+        />
     );
 };
 
