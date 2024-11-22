5d44f7d71ec41f1a2608ded6eb8ab5501e3baa2d
Andrew <drejsp@gmail.com>
2024-11-21 22:31:34 -0500

    added a bunch of refactoring in regards to styling, added a new menuItem component for modularization fixed IconButton component and is now able to open and close

--- multitwitch/src/components/MoreOptionsMenu.jsx
+++ multitwitch/src/components/MoreOptionsMenu.jsx
@@ -1,53 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { MoreIcon, LanguageIcon, KeyboardIcon, SettingsIcon, InfoIcon, ChevronRightIcon, MoonIcon } from '../assets/Icons';
 import IconButton from './IconButton';
 import ThemeToggleButton from './ThemeToggleButton';
 import { themes } from '../config/theme';
+import MenuItem from './MenuItem';
 
 const MoreOptionsMenu = ({ theme, onThemeToggle }) => {
   const currentTheme = themes[theme.name];
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
-    <div className="relative inline-block text-left">
+    <div ref={menuRef} className="relative inline-block text-left">
       {/* More Options Button */}
       <IconButton
         icon={MoreIcon}
         ariaLabel="More options"
-        className="ml-4 mr-2 cursor-pointer hover:bg-[var(--hoverBackground)] rounded p-2 text-gray-400"
+        onClick={toggleMenu}
+        className="ml-4 mr-2 cursor-pointer hover:bg-[var(--hover-background)] rounded p-2 text-[var(--primary-text)]"
       />
       
       {/* Dropdown Menu */}
-      <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-md bg-[${currentTheme.colors.secondaryBackground}] ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-          <div className="flex items-center justify-between px-4 py-2 text-sm text-gray-300 hover:bg-[${currentTheme.colors.hoverBackground}] cursor-pointer" role="menuitem">
-            <div className="flex items-center">
-              <MoonIcon className="mr-2 w-5 h-5" />
-              <span>Theme</span>
+      {/* Conditional rendering the options menu, will be only executed when isOpen is true. Wont add any unnecessary clutter in the dom */}
+      {isOpen && (
+        <div
+          className="bg-[var(--secondary-background)] origin-top-right absolute right-2 mt-2 w-48 rounded-md ring-1 ring-black ring-opacity-5 focus:outline-none"
+        >
+          {/* allowing padding for options */}
+          <div className="px-2 py-1">  
+          {/* Theme Option */}
+            <div className="py-1" role="menu" area-orientation="vertical" aria-lavelledby="options-menu">
+              <MenuItem 
+                icon={MoonIcon}
+                label='Theme'
+                rightIcon={ThemeToggleButton}
+                rightIconProps={{ theme: theme.name, onToggle: onThemeToggle, className: "w-8 h-4" }}
+                noHover
+              />
+              <MenuItem 
+                icon={LanguageIcon}
+                label='Language'
+                rightIcon={ChevronRightIcon}
+                rightIconProps={{ className: "w-4 h-4"}}
+              />
+              <MenuItem 
+                icon={KeyboardIcon}
+                label='Keyboard Shortcuts'
+              />
+              <MenuItem 
+                icon={SettingsIcon}
+                label="Settings"
+              />
+              <hr className="w-40 mx-auto my-2 border-[var(--border-default)]" />
+              <MenuItem
+                icon={InfoIcon}
+                label="Info"
+              />
             </div>
-            <ThemeToggleButton theme={theme.name} onToggle={onThemeToggle} className="w-8 h-4" />
-          </div>
-          <div className="flex items-center justify-between px-4 py-2 text-sm text-gray-300 hover:bg-[${currentTheme.colors.hoverBackground}] cursor-pointer" role="menuitem">
-            <div className="flex items-center">
-              <LanguageIcon className="mr-2 w-5 h-5" />
-              <span>Language</span>
-            </div>
-            <ChevronRightIcon className="w-4 h-4 text-gray-500" />
-          </div>
-          <div className="flex items-center px-4 py-2 text-sm text-gray-300 hover:bg-[${currentTheme.colors.hoverBackground}] cursor-pointer" role="menuitem">
-            <KeyboardIcon className="mr-2 w-5 h-5" />
-            <span>Keyboard Shortcuts</span>
-          </div>
-          <div className="flex items-center px-4 py-2 text-sm text-gray-300 hover:bg-[${currentTheme.colors.hoverBackground}] cursor-pointer" role="menuitem">
-            <SettingsIcon className="mr-2 w-5 h-5" />
-            <span>Settings</span>
-          </div>
-          <hr className="w-40 mx-auto border-opacity-30 border-gray-600 my-2" />
-          <div className="flex items-center px-4 py-2 text-sm text-gray-300 hover:bg-[${currentTheme.colors.hoverBackground}] cursor-pointer" role="menuitem">
-            <InfoIcon className="mr-2 w-5 h-5" />
-            <span>Info</span>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
